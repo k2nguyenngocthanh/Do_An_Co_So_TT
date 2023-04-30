784e94310a695f4adcc8c7ad9ee4f97ffc309f2f
Nguyen Thanh Tuan <nguyenthanhtuan300102@gmail.com>
2023-04-30 12:09:29 +0700

    update login - register

---
+++ SignUp/login.js
@@ -0,0 +1,36 @@
+let apiUser = "http://localhost:3000/user";
+
+//login
+const confirmpassword = document.querySelector(".input-signup-confirmpassword");
+const mail = document.querySelector(".input-signup-mail");
+const username = document.querySelector(".input-login-username");
+const password = document.querySelector(".input-login-password");
+const bntLogin = document.querySelector(".login__signInButton");
+
+// get user
+const getUser = async () => {
+  const response = await fetch(apiUser);
+  const data = await response.json();
+  return data;
+};
+
+// login
+bntLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (username.value == "" || password.value == "") {
+    alert("Vui lòng không để trống");
+  } else {
+    getUser().then((data) => {
+      const user = data.find(
+        (user) =>
+          user.username == username.value && user.password == password.value
+      );
+      if (user) {
+        alert("Đăng nhập thành công");
+        window.location.href = " ../index.html";
+      } else {
+        alert("Đăng nhập thất bại");
+      }
+    });
+  }
+});
